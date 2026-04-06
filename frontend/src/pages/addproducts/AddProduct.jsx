5aed8c64693502b74ef82f4a7ed5ad66e818f89a
dev0802 <devsehgal121@gmail.com>
2026-04-06 21:09:19 +0530

    Fixed UI for Main Page and Add Product Page

--- frontend/src/pages/addproducts/AddProduct.jsx
+++ frontend/src/pages/addproducts/AddProduct.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
 export default function AddProduct() {
     const [showPopup, setShowPopup] = useState(false);
+
+    const numericInput = (e) => {
+        const value = e.target.value.replace(/[^0-9.]/g, '');
+        // Prevent multiple decimal points
+        e.target.value = value.split('.').length > 2 ? value.replace(/\.+$/, '') : value;
+        e.target.addEventListener('blur', (e) => {
+            e.target.value = e.target.value.replace(/\.+$/, '');
+        });
+    };
     return (
         <div className=' w-full'>
-            <div className='bg-gradient-to-r from-gray-300 to-gray-500 shadow-lg shadow-gray-600 backdrop-blur-md border border-gray-400 rounded-2xl shadow-lg p-6 md:max-w-7xl md:min-h-4xl mx-auto '>
+            <div className='bg-gradient-to-r from-gray-300 to-gray-400 shadow-lg shadow-gray-600 backdrop-blur-md border border-gray-400 rounded-2xl shadow-lg p-6 md:max-w-7xl md:min-h-4xl mx-auto '>
                 <h2 className='text-gray-700 text-2xl font-bold mb-3'>Add Product</h2>
                 <div className='flex flex-col gap-3'>
                     {/* Item Description */}
@@ -17,87 +26,117 @@ export default function AddProduct() {
                         {/* Popup for adding new item description */}
                         <div className='relative'>
                             <button
-                            onClick={() => setShowPopup(!showPopup)} 
-                            className="peer px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none hover:shadow-md">+</button>
+                                onClick={() => setShowPopup(!showPopup)}
+                                className=" bg-transparent outline-none flex items-center justify-center text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" className='h-6 w-6' width="29" height="20" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                                    <path d="M12 5v14M5 12h14" />
+                                </svg>
+                            </button>
                             {showPopup && (
-                            <div className='absolute left-0 top-12 z-10 bg-gradient-to-r from-gray-300 to-gray-500 border border-gray-400 rounded-2xl shadow-lg p-4 w-80'>
-                                <h3 className='text-gray-700 text-md font-bold mb-3'>Item Description</h3>
-                                <input
-                                    className='w-full px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md mb-3'
-                                    placeholder='Enter item description'
-                                />
-                                <button onClick={() => setShowPopup(!showPopup)} className='w-full text-white text-sm font-semibold px-4 py-2 rounded-full border border-white/40 bg-gradient-to-r from-gray-600 to-gray-700 hover:opacity-80 transition-all duration-200'>
-                                    Add Item
-                                </button>
-                            </div>
-                        )}
+                                <div className='absolute left-0 top-12 z-10 bg-gradient-to-r from-gray-300 to-gray-500 border border-gray-400 rounded-2xl shadow-lg p-4 w-80'>
+                                    <h3 className='text-gray-700 text-md font-bold mb-3'>Item Description</h3>
+                                    <input
+                                        className='w-full px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md mb-3'
+                                        placeholder='Enter item description'
+                                    />
+                                    <button onClick={() => setShowPopup(!showPopup)} className='w-full text-white text-sm font-semibold px-4 py-2 rounded-full border border-white/40 bg-gradient-to-r from-gray-600 to-gray-700 hover:opacity-80 transition-all duration-200'>
+                                        Add Item
+                                    </button>
+                                </div>
+                            )}
                         </div>
                         {/* Delete button for item description */}
-                        <button className="px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none hover:shadow-md">-</button>
+                        <button className="text-red-700  p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                        </button>
                     </div>
                     {/* HSN Code */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>HSN Code</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Gross Weight */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Gross Weight (Gms.)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Stone Weight */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Stone Weight (Gms.)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Moti Weight */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Moti Weight (Gms.)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Diamond Weight */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Diamond Weight (Ct.)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Solitaire Weight */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Solitaire Weight (Ct.)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Color Stone */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Color Stone (Gms.)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Minna */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Minna (Gms.)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Colouring */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Colouring (Gms.)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Net Weight */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Net Weight (Gms.)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
                     {/* Price */}
                     <div className='flex items-center gap-3'>
                         <label className='text-gray-700 text-md font-medium w-44 '>Price (₹)</label>
-                        <input className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
+                        <input
+                            onChange={numericInput}
+                            className='flex-1 px-4 py-2 rounded-full border border-gray-400 bg-white/50 outline-none focus:border-gray-400 focus:shadow-md from-shadow-gray-300 to-shadow-gray-400' />
                     </div>
-                    
-                    <div className="mt-2 ">
-                        <button className='text-white text-sm hover:shadow-xl shadow-gray-700 font-semibold px-4 py-2 rounded-full border border-white/40 bg-gradient-to-r from-gray-600 to-gray-700  transition-all duration-200'>
+
+                    <div className="mt-2 flex justify-end">
+                        <button
+                            type='reset'
+                            className='text-white text-sm hover:shadow-xl shadow-gray-700 font-semibold px-4 py-2 rounded-full border border-white/40 bg-gradient-to-r from-gray-600 to-gray-700  transition-all duration-200'>
                             Reset
                         </button>
-                        <button className='text-white text-sm hover:shadow-xl shadow-gray-700 font-semibold px-4 py-2 rounded-full border border-white/40 bg-gradient-to-r from-gray-600 to-gray-700 hover:bg-gradient-to-r hover:from-gray-700 hover:to-gray-800 transition-all duration-200 ml-3'>
-                            Print only Label
-                        </button>
+                        
                         <button className='text-white text-sm hover:shadow-xl shadow-gray-700 font-semibold px-4 py-2 rounded-full border border-white/40 bg-gradient-to-r from-gray-600 to-gray-700 hover:bg-gradient-to-r hover:from-gray-700 hover:to-gray-800 transition-all duration-200 ml-3'>
                             Submit
                         </button>
--- frontend/src/pages/home/MainPage.jsx
+++ frontend/src/pages/home/MainPage.jsx
@@ -2,46 +2,49 @@ import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 export default function MainPage() {
   return (
-    <div className='bg-gray-300 min-h-screen p-2 '>
+    <div className='bg-gray-400 min-h-screen p-2 '>
       {/* Header */}
-      <header className='w-full flex items-center justify-between bg-gradient-to-r from-gray-700 to-gray-800 px-4 md:px-8 py-3 md:py-4 shadow-lg'>
+      <header className='w-full flex items-center justify-between bg-gray-400 px-4 md:px-8 py-3 md:py-4'>
         <h1 className='text-white text-sm md:text-xl font-bold tracking-widest uppercase'>BrandEase</h1>
-        <h2 className='text-white text-base md:text-2xl font-semibold tracking-wide'>Inventra</h2>
+        {/* <h2 className='text-white text-base md:text-2xl font-semibold tracking-wide'>Inventra</h2> */}
+        <div className='w-full flex justify-center'>
+          <nav className='flex flex-row justify-center gap-1 sm:gap-5 bg-transparent'>
+            {/* Add Products */}
+
+              <Link to='/main/addproducts'>
+            {/* <div className='flex hover:text-gray-400 hover:underline'> */}
+                <button
+                  className='relative text-center text-white text-lg after:absolute after:left-0 after:centre after:bottom-0 after:h-[3px] after:w-0 after:bg-gray-500 after:transition-all after:duration-300 hover:after:w-full hover:text-gray-500 font-semibold px-1 md:px-4 py-1.5 md:py-2  transition-all duration-200 whitespace-nowrap'          >
+                  Add Products
+                </button>
+            
+              </Link>
+            {/* View Products */}
+            <button
+                  className='relative text-center text-white text-lg after:absolute after:left-0 after:centre after:bottom-0 after:h-[3px] after:w-0 after:bg-gray-500 after:transition-all after:duration-300 hover:after:w-full hover:text-gray-500 font-semibold px-1 md:px-4 py-1.5 md:py-2  transition-all duration-200 whitespace-nowrap'          >
+            
+              View Products
+            </button>
+            {/* Print Invoice */}
+            <button
+                                className='relative text-center text-white text-lg after:absolute after:left-0 after:centre after:bottom-0 after:h-[3px] after:w-0 after:bg-gray-500 after:transition-all after:duration-300 hover:after:w-full hover:text-gray-500 font-semibold px-1 md:px-4 py-1.5 md:py-2  transition-all duration-200 whitespace-nowrap'          >
+
+              Print Invoice
+            </button>
+            {/* Report */}
+            <button
+              className='relative text-center text-white text-lg after:absolute after:left-0 after:centre after:bottom-0 after:h-[3px] after:w-0 after:bg-gray-500 after:transition-all after:duration-300 hover:after:w-full hover:text-gray-500 font-semibold px-1 md:px-4 py-1.5 md:py-2  transition-all duration-200 whitespace-nowrap'
+            >
+              Report
+            </button>
+          </nav>
+        </div>
         <button className='text-white text-xs md:text-sm font-semibold px-2 md:px-4 py-1 md:py-2 rounded-full border border-white/40 bg-white/10 hover:bg-white/25 transition-all duration-200'>
           John
         </button>
       </header>
       {/* Navigation Bar*/}
-      <div className='w-full flex justify-center'>
-        <nav className='flex flex-row justify-center gap-1 sm:gap-5 bg-gradient-to-r from-gray-600 to-gray-700 border border-gray-400 rounded-full shadow-lg p-1 md:p-4 mt-3 w-full max-w-2xl mx-auto'>
-          {/* Add Products */}
-          <Link to='/main/addproducts'>
-            <button
-              className='text-center text-white text-[10px] md:text-sm font-semibold px-2 md:px-4 py-1.5 md:py-2 rounded-full border border-white/40 bg-white/10 hover:bg-white/25 transition-all duration-200 whitespace-nowrap'          >
-              Add Products
-            </button>
-          </Link>
-          {/* View Products */}
-          <button
-            className='text-white text-xs md:text-sm font-semibold px-3 md:px-4 py-1.5 md:py-2 rounded-full border border-white/40 bg-white/10 hover:bg-white/25 transition-all duration-200 whitespace-nowrap'
-          >
-            View Products
-          </button>
-          {/* Print Invoice */}
-          <button
-            className='text-white text-xs md:text-sm font-semibold px-3 md:px-4 py-1.5 md:py-2 rounded-full border border-white/40 bg-white/10 hover:bg-white/25 transition-all duration-200 whitespace-nowrap'
-          >
-            Print Invoice
-          </button>
-          {/* Report */}
-          <button
-            className='text-white text-xs md:text-sm font-semibold px-3 md:px-4 py-1.5 md:py-2 rounded-full border border-white/40 bg-white/10 hover:bg-white/25 transition-all duration-200 whitespace-nowrap'
-          >
-            Report
-          </button>
-        </nav>
-      </div>
-      
+
       <main className='flex-1 p-4'>
         <Outlet />
       </main>
